0e66ec9b107202e9dbad2ae205cf746a06588e55
thomasdindin <thomas.dindin@gmail.com>
2024-01-30 14:37:11 +0100

    :sparkles: New features : Now connected to the API.

--- lib/Services/api_services.dart
+++ lib/Services/api_services.dart
@@ -1,23 +1,128 @@
-import 'package:http/http.dart' as http;
+import 'dart:async';
 import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
 
 class ApiService {
-  final String baseUrl;
+  final String baseIp = "192.168.107";
+  final String port = "8000";
+  final String baseApiUrl = "/api/";
 
-  ApiService({required this.baseUrl});
+  final Map<String, String> headers = {
+    'Accept': 'application/ld+json',
+  };
 
-  Future<List<dynamic>> fetchItems() async {
+
+  //TODO: Changer ça pour avoir le bon ip. Penser à se connecter sur la 107.
+  final String dynamicIp = "106";
+
+  ApiService();
+
+  Future<List<dynamic>> get(String table, [int? id]) async {
     try {
-      final response = await http.get(Uri.parse(baseUrl));
-      if (response.statusCode == 200) {
-        return json.decode(response.body);
+      http.Response response;
+
+
+      if (id == null) {
+        response = await http.get(
+          Uri.parse("http://$baseIp.$dynamicIp:$port$baseApiUrl$table"),
+          headers: headers,
+        );
       } else {
-        // Gérer les erreurs ici
+        response = await http.get(
+          Uri.parse("http://$baseIp.$dynamicIp:$port$baseApiUrl$table/$id"),
+          headers: headers,
+        );
+      }
+
+      if (response.statusCode == 200) {
+        // Vérifiez si le body est une liste
+        var jsonData = json.decode(response.body);
+        if (jsonData is List) {
+          return jsonData;
+        } else if (jsonData is Map && jsonData.containsKey('hydra:member')) {
+          // Si le jsonData est une Map et contient la clé 'hydra:member',
+          // nous retournons cette liste.
+          return jsonData['hydra:member'] as List;
+        } else {
+          throw Exception('Invalid JSON format');
+        }
+      } else {
         throw Exception('Failed to load items');
       }
+    } on TimeoutException catch (_) {
+      throw Exception('Request timed out');
     } catch (e) {
-      // Gérer les exceptions ici
       throw Exception('Failed to load items: $e');
+    }
+  }
+
+
+  Future<void> post(String table, Map<String, dynamic> data) async {
+    try {
+      final response = await http.post(
+        Uri.parse("http://$baseIp.$dynamicIp:$port$baseApiUrl$table"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode != 201) {
+        throw Exception('Failed to create item');
+      }
+    } catch (e) {
+      throw Exception('Failed to create item: $e');
+    }
+  }
+
+  Future<void> put(String table, int id, Map<String, dynamic> data) async {
+    try {
+      final response = await http.put(
+        Uri.parse("http://$baseIp.$dynamicIp:$port$baseApiUrl$table/$id"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to update item');
+      }
+    } catch (e) {
+      throw Exception('Failed to update item: $e');
+    }
+  }
+
+  Future<void> patch(String table, int id, Map<String, dynamic> data) async {
+    try {
+      final response = await http.patch(
+        Uri.parse("http://$baseIp.$dynamicIp:$port$baseApiUrl$table/$id"),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to update item');
+      }
+    } catch (e) {
+      throw Exception('Failed to update item: $e');
+    }
+  }
+
+  Future<void> delete(String table, int id) async {
+    try {
+      final response = await http.delete(
+        Uri.parse("http://$baseIp.$dynamicIp:$port$baseApiUrl$table/$id"),
+      );
+
+      if (response.statusCode != 204) {
+        throw Exception('Failed to delete item');
+      }
+    } catch (e) {
+      throw Exception('Failed to delete item: $e');
     }
   }
 }
